8147d67316699751b23cc1d8b66ce81c04e1c1fe
Khifayat <abolurinkhifayat1@gmail.com>
2021-01-05 14:29:00 -0500

    day 4 of coding, didnt push day 1-3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const courses = [
 ];
 
 app.get('/', (req,res)=>{
-res.send("Hello World")
+res.render('index', {title: 'My Express App', message: 'Hello'});
 });
 
 app.get('/api/courses', (req,res) => {
